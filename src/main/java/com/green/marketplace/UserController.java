54cd6485c81a998dfd260ec957fb2eb465fa9f2d
Jamie Cronin <jamie.cronin@ucdconnect.ie>
2023-02-15 19:08:02 +0000

    Add user related pages.

---
+++ src/main/java/com/green/marketplace/UserController.java
@@ -0,0 +1,36 @@
+package com.green.marketplace;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UserController {
+
+	@GetMapping("/profile")
+	public String profile(@RequestParam(name="type", required=false, defaultValue="user") String name, Model model) {
+        if (name == "admin") {
+            model.addAttribute("page", "Profile");
+		    return "user/profile";
+        } else {
+            model.addAttribute("page", "Home");
+		    return "user/dashboard";
+        }
+		
+	}
+
+    @GetMapping("/login")
+	public String login(Model model) {
+		model.addAttribute("page", "Log In");
+		return "user/login";
+	}
+    
+
+    @GetMapping("/signup")
+	public String home(Model model) {
+		model.addAttribute("page", "Sign Up");
+		return "user/signup";
+	}
+
+}
