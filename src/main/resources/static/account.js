b7d776c9387fca9ec7535f20808d5f478b702270
loidtayag <loidtayag@gmail.com>
2023-03-03 18:13:35 +0000

    Fixed bug due not accounting for duplicate id names across html files

--- src/main/resources/static/account.js
+++ src/main/resources/static/account.js
@@ -2,7 +2,7 @@ document.getElementById("delete_session").addEventListener("click", () => {
     document.cookie = 'sessionID=null; expires=' + new Date(1999, 11, 30).toUTCString();
 })
 
-document.getElementById("username")?.addEventListener("change", async e => {
+document.getElementsByClassName("duplicate")[0]?.addEventListener("change", async e => {
     let valid = await fetch(`http://localhost:8080/validateUsername?username=${e.target.value}`);
     valid = await valid.text()
     if (valid === "false") {
