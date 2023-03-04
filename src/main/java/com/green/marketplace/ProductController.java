cbabb51c3b98161e812424de517a2ac950447f8e
Nicky <nikita.danilov@ucdconnect.ie>
2023-03-04 22:54:26 +0000

    cleaning up code

--- src/main/java/com/green/marketplace/ProductController.java
+++ src/main/java/com/green/marketplace/ProductController.java
@@ -9,7 +9,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
